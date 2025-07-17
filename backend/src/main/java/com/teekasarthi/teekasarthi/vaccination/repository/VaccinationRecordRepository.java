a2d940bbe93dbf8537b8f0b79955baec67899ca5
Vishesh-04 <visheshjindal04@gmail.com>
2025-07-17 20:41:12 +0530

    Merge Conflicts resolved

--- backend/src/main/java/com/teekasarthi/teekasarthi/vaccination/repository/VaccinationRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.teekasarthi.teekasarthi.vaccination.repository;
-
-import com.teekasarthi.teekasarthi.vaccination.entity.VaccinationRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VaccinationRecordRepository extends JpaRepository<VaccinationRecord, Long> {
-}
